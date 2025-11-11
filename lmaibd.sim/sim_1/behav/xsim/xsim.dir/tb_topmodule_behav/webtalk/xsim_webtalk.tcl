015f0585fa3e0a2b9438c47c23fc9d6efce09f8d
jk <jetandark404@gmail.com>
2025-11-11 12:56:26 -0800

    Added readme.md

--- lmaibd.sim/sim_1/behav/xsim/xsim.dir/tb_topmodule_behav/webtalk/xsim_webtalk.tcl
+++ lmaibd.sim/sim_1/behav/xsim/xsim.dir/tb_topmodule_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/godso/lmaibd/lmaibd.sim/sim_1/behav/xsim/xsim.dir/tb_topmodule_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sun Nov  9 04:21:01 2025" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sun Nov  9 04:35:01 2025" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "9fe71592-e7e5-410d-a8fc-b5e428d83caf" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "133ee3e4c6114b4eaab0e455a23d851d" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "86" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "87" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD A6-5200 APU with Radeon(TM) HD Graphics    " -context "user_environment"
@@ -27,6 +27,6 @@ webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6628_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3490635609 -regid "" -xml C:/Users/godso/lmaibd/lmaibd.sim/sim_1/behav/xsim/xsim.dir/tb_topmodule_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/godso/lmaibd/lmaibd.sim/sim_1/behav/xsim/xsim.dir/tb_topmodule_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/godso/lmaibd/lmaibd.sim/sim_1/behav/xsim/xsim.dir/tb_topmodule_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6624_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 4060116915 -regid "" -xml C:/Users/godso/lmaibd/lmaibd.sim/sim_1/behav/xsim/xsim.dir/tb_topmodule_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/godso/lmaibd/lmaibd.sim/sim_1/behav/xsim/xsim.dir/tb_topmodule_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/godso/lmaibd/lmaibd.sim/sim_1/behav/xsim/xsim.dir/tb_topmodule_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
