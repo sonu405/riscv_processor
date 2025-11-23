1ceb520c390267813c8c2a8eaa1f55660b8a1e50
jk <jetandark404@gmail.com>
2025-11-23 12:06:16 -0800

    Fixed the datapath by performing jalr with the PCTarget Adder instead of ALU and also removed the PCSrc control signal. Instead using combinational logic using branch and Jump control signals to drive the mux before PC

--- lmaibd.srcs/sources_1/new/control_logic.sv
+++ lmaibd.srcs/sources_1/new/control_logic.sv
@@ -2,9 +2,8 @@
 
 module control_logic(
 input logic [6:0] opcode,
-input logic Zero,
-output logic Branch, MemRead, MemWrite, ALUSrc, RegWrite,
-output logic [1:0] ALUOp, ImmSrc, ResultSrc, PCSrc);
+output logic Branch, MemRead, MemWrite, ALUSrc, RegWrite, Jump, 
+output logic [1:0] ALUOp, ImmSrc, ResultSrc);
 // TODO: REMOVE MemRead after confirmation
 // ResultSrc replaces MemToReg
 
@@ -19,7 +18,7 @@ always_comb begin
             ALUOp = 2'b10;
             ImmSrc = 0; // Don't Care
             ResultSrc = 2'b00;
-            PCSrc = 2'b00;
+            Jump = 0;
         end
         7'b0010011: begin // I type other than load and jalr
             RegWrite = 1;
@@ -30,7 +29,7 @@ always_comb begin
             ALUOp = 2'b10;
             ImmSrc = 2'b00;
             ResultSrc = 2'b00; // Write alu out to register file
-            PCSrc = 2'b00;
+            Jump = 0;
         end
         7'b0000011: begin   // I type -- load instructions
             RegWrite = 1;
@@ -41,7 +40,7 @@ always_comb begin
             ALUOp = 2'b00;
             ImmSrc = 2'b00;
             ResultSrc = 2'b01; // write memory to register file
-            PCSrc = 2'b00;
+            Jump = 1'b0;
         end
         7'b1100111: begin   // I type -- Jump and Link Register
             RegWrite = 1;
@@ -52,7 +51,7 @@ always_comb begin
             ALUOp = 2'b00;  // ADD
             ImmSrc = 2'b00;
             ResultSrc = 2'b10; // write PC+4 to register file
-            PCSrc = 2'b10;
+            Jump = 1;            
         end
         7'b0100011: begin  // S TYPE
             RegWrite = 0;
@@ -63,7 +62,7 @@ always_comb begin
             ALUOp = 2'b00;
             ImmSrc = 2'b01; 
             ResultSrc = 2'b00; // Don't care, we set to 0
-            PCSrc = 2'b00;
+            Jump = 0;
         end
         7'b1100011: begin   // B type
             RegWrite = 0;
@@ -74,8 +73,7 @@ always_comb begin
             ALUOp = 2'b01;
             ImmSrc = 2'b10;
             ResultSrc = 2'b00; // DON't care
-            if (Zero)
-                PCSrc = 2'b01; // only when the instruction is B type and alu's Zero bit is 1
+            Jump = 0;
         end
         7'b1101111:begin       // J Type
             RegWrite = 1;      // We write PC+4 to register file
@@ -86,7 +84,7 @@ always_comb begin
             ALUOp = 2'b00;     // we don't use alu
             ImmSrc = 2'b11;
             ResultSrc = 2'b10; // write PC+4 to register file
-            PCSrc = 2'b01;      // take PCTarget as PCNext 
+            Jump = 1;
         end        
     endcase
 end
--- lmaibd.srcs/sources_1/new/topmodule.sv
+++ lmaibd.srcs/sources_1/new/topmodule.sv
@@ -20,16 +20,25 @@ logic [31:0] alu_out, mux_out;
 logic [31:0] Result;
 
 // CONTROL LOGIC
-logic Branch, MemRead, MemWrite, ALUSrc, RegWrite;
-logic [1:0] ALUOp, ImmSrc, ResultSrc, PCSrc;
+logic Branch, MemRead, MemWrite, ALUSrc, RegWrite, Jump;
+logic [1:0] ALUOp, ImmSrc, ResultSrc;
 logic [3:0] alu_control_lines;
 
-// Program Counter 
+// Program Counter Logic for B and J type instructions
 adder uut_add1(.in1(pc), .in2(4), .out(PCPlus4)); // adder for => PC + 4
-adder uut_add2(.in1(pc), .in2(extended_imm), .out(PCTarget)); // calculates PCTarget for B and J type instructions
 
-fourby1mux uut_mux2(.in1(PCPlus4), .in2(PCTarget), .in3(alu_out), .in4(32'b0), .BSel(PCSrc), .out(PCNext));
+// This mux is 1 for jalr or else 0. 
+logic [31:0] PCTargetAdderInput;
 
+// deciding either to do PC + imm or data1 + imm.
+// data1 comes from register file. Note that, we only select data1 for jalr instruction.
+mux uut_mux4(.in1(pc), .in2(data1), .BSel(Jump & (~opcode[3])), .out(PCTargetAdderInput));
+
+// calculating PCTarget for jalr, jalr and branch
+adder uut_add2(.in1(extended_imm), .in2(PCTargetAdderInput), .out(PCTarget)); // calculates PCTarget for B and J type instructions
+
+// mux before PC
+mux uut_mux3(.in1(PCPlus4), .in2(PCTarget), .BSel((ALUFlags[2] & Branch) | Jump), .out(PCNext));
 progcounter pc_uut(.clk(clk), .rst(rst), .PCNext(PCNext),.pc(pc));
 
 // Instruction memory
@@ -46,9 +55,9 @@ always_comb begin
 end
 
 // CONTROL LOGIC
-control_logic uut_control_unit(.opcode(opcode), .Zero(ALUFlags[1]), .Branch(Branch), .MemRead(MemRead),
+control_logic uut_control_unit(.opcode(opcode), .Branch(Branch), .MemRead(MemRead),
  .MemWrite(MemWrite), .ALUSrc(ALUSrc), .RegWrite(RegWrite), .ImmSrc(ImmSrc), 
- .PCSrc(PCSrc), .ResultSrc(ResultSrc), .ALUOp(ALUOp));
+ .Jump(Jump), .ResultSrc(ResultSrc), .ALUOp(ALUOp));
  
 // ALU CONTROL
 alu_control uut_alu_control(.ALUOp(ALUOp), .funct3(funct3), .op5(opcode[5]), .funct7(funct7[5]), .alu_control_lines(alu_control_lines));
